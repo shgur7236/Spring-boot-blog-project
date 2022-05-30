db633b8ad5f060a36381e215026c591a6646f11b
shgur7236 <shgurtns7236@naver.com>
2022-05-30 23:47:39 +0900

    UserApiController 회원가입

---
+++ src/main/java/blogproject/blog/controller/api/UserApiController.java
@@ -0,0 +1,19 @@
+package blogproject.blog.controller.api;
+
+import blogproject.blog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/v1/user")
+    public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
+        return userService.save(userSaveRequestDto.toEntity());
+    }
+}
