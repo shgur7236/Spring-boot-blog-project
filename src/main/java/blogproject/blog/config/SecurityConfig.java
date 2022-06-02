97b19d9f25b36854d2467fe175faf3a601afea44
shgur7236 <shgurtns7236@naver.com>
2022-06-02 16:06:51 +0900

    password 암호화

--- src/main/java/blogproject/blog/config/SecurityConfig.java
+++ src/main/java/blogproject/blog/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package blogproject.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
