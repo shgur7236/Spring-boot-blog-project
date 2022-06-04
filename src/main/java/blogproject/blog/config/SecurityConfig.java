586d1f4222738157ba373779fd3d96d6a43ea388
shgur7236 <shgurtns7236@naver.com>
2022-06-04 15:29:38 +0900

    오류 수정

--- src/main/java/blogproject/blog/config/SecurityConfig.java
+++ src/main/java/blogproject/blog/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package blogproject.blog.config;
 
 import blogproject.blog.config.auth.PrincipalDetail;
+import blogproject.blog.config.auth.PrincipalDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
