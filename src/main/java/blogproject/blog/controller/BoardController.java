8e53c194cc9bd527ac28fb9f6b4f482f7bda2cfa
shgur7236 <shgurtns7236@naver.com>
2022-06-05 18:01:11 +0900

    글쓰기 컨트롤러 구현

---
+++ src/main/java/blogproject/blog/controller/BoardController.java
@@ -0,0 +1,13 @@
+package blogproject.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/save")
+    public String save(){
+        return "layout/board/board-save";
+    }
+}
