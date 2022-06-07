5121e431928501477bbf969b1866ec92469c42d9
shgur7236 <shgurtns7236@naver.com>
2022-06-07 20:36:21 +0900

    상세보기 페이지 controller 구현

--- src/main/java/blogproject/blog/controller/BoardController.java
+++ src/main/java/blogproject/blog/controller/BoardController.java
@@ -1,13 +1,26 @@
 package blogproject.blog.controller;
 
+import blogproject.blog.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+    private final BoardService boardService;
 
     @GetMapping("/board/save")
     public String save(){
         return "layout/board/board-save";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.detail(id));
+        return "layout/board/board/board-detail";
     }
 }
