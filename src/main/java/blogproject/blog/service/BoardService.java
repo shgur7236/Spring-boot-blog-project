507db4d562f73f3b58c50d893cf3a5213d3ef944
shgur7236 <shgurtns7236@naver.com>
2022-06-07 16:17:05 +0900

    글목록 로직 findAll()추가

--- src/main/java/blogproject/blog/service/BoardService.java
+++ src/main/java/blogproject/blog/service/BoardService.java
@@ -1,11 +1,14 @@
 package blogproject.blog.service;
 
+import blogproject.blog.domain.board.Board;
 import blogproject.blog.domain.board.BoardRepository;
 import blogproject.blog.domain.user.User;
 import blogproject.blog.dto.board.BoardSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +20,12 @@ public class BoardService {
     public Long save(BoardSaveRequestDto boardSaveRequestDto, User user){
         boardSaveRequestDto.setUser(user);
         return boardRepository.save(boardSaveRequestDto.toEntity()).getId();
+    }
+
+    /**
+     * 글목록 로직
+     */
+    public List<Board> findAll(){
+        return boardRepository.findAll();
     }
 }
