82acf5a9d80c6e5543bead5451589e10ab72ee02
shgur7236 <shgurtns7236@naver.com>
2022-06-08 19:59:34 +0900

    JPA이용한 RESTful API작성

--- src/main/java/blogproject/blog/repository/BoardRepository.java
+++ src/main/java/blogproject/blog/repository/BoardRepository.java
@@ -4,6 +4,9 @@ package blogproject.blog.repository;
 import blogproject.blog.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitleOrContent(String title, String content);
 }
