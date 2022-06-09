62d5378b94352dac43dba4e5d4706b06cd2e0fd4
shgur7236 <shgurtns7236@naver.com>
2022-06-09 16:58:07 +0900

    page

--- src/main/java/blogproject/blog/controller/BoardController.java
+++ src/main/java/blogproject/blog/controller/BoardController.java
@@ -6,6 +6,7 @@ import blogproject.blog.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,8 +29,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model, Pageable pageable) {
-        Page<Board> boards = boardRepository.findAll(pageable);
+    public String list(Model model,@PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "")String searchText) {
+     //   Page<Board> boards = boardRepository.findAll(pageable);
+          Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+        int startPage = Math.max(0, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
         model.addAttribute("boards",boards);
         return "board/list";
     }
--- src/main/java/blogproject/blog/repository/BoardRepository.java
+++ src/main/java/blogproject/blog/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package blogproject.blog.repository;
 
 
 import blogproject.blog.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
