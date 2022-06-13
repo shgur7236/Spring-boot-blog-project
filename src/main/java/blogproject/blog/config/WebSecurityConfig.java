481c7a61e06b3a6da4be4fd04b0220d2c3e3033d
shgur7236 <shgurtns7236@naver.com>
2022-06-13 15:37:21 +0900

    login

--- src/main/java/blogproject/blog/config/WebSecurityConfig.java
+++ src/main/java/blogproject/blog/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public  class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/account/login")
                 .permitAll()
                 .and()
                 .logout()
