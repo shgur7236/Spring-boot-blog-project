790748a548a24bf4d6f02e8e96101af4bdac65cb
shgur7236 <shgurtns7236@naver.com>
2022-06-13 17:20:41 +0900

    login

--- src/main/java/blogproject/blog/config/WebSecurityConfig.java
+++ src/main/java/blogproject/blog/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public  class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/css/**").permitAll()
+                .antMatchers("/","/css/**","/account/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -44,10 +44,10 @@ public  class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .usersByUsernameQuery("select username, password, enabled "
                         + "from user "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select username, name "
+                .authoritiesByUsernameQuery("select u.username, r.name "
                         + "from user_role ur inner join user u on ur.user_id = u.id "
-                        + "inner join role r on ur.id = r.id "
-                        + "where email = ?");
+                        + "inner join role r on ur.role_id = r.id "
+                        + "where u.username = ?");
     }
 
     // static을 추가
