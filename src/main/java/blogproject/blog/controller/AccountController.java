0a3cf956953de44841a7ae35c74ccde3744e0e7f
shgur7236 <shgurtns7236@naver.com>
2022-06-13 17:20:58 +0900

    login, register

--- src/main/java/blogproject/blog/controller/AccountController.java
+++ src/main/java/blogproject/blog/controller/AccountController.java
@@ -1,17 +1,36 @@
 package blogproject.blog.controller;
 
+import blogproject.blog.model.User;
+import blogproject.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login(){
         return "account/login";
     }
 
-    @PostMapping("/register")
+    @GetMapping("/register")
     public String register(){
+
         return "account/register";
     }
+
+    @PostMapping("/register")
+    public String register(User user){
+        userService.save(user);
+        return "redirect:/";
+    }
+
+
 }
