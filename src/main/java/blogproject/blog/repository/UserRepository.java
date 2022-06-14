7f4affbc2761eb8126001b02dc3edc4c643e65e7
shgur7236 <shgurtns7236@naver.com>
2022-06-14 10:33:27 +0900

    findByUsername

--- src/main/java/blogproject/blog/repository/UserRepository.java
+++ src/main/java/blogproject/blog/repository/UserRepository.java
@@ -4,4 +4,5 @@ import blogproject.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
