0f03d0d263e65c3da5962b8bb8cbbb5939630ea3
shgur7236 <shgurtns7236@naver.com>
2022-06-14 10:36:44 +0900

    +BoardService

--- src/main/java/blogproject/blog/controller/BoardController.java
+++ src/main/java/blogproject/blog/controller/BoardController.java
@@ -2,11 +2,13 @@ package blogproject.blog.controller;
 
 import blogproject.blog.model.Board;
 import blogproject.blog.repository.BoardRepository;
+import blogproject.blog.service.BoardService;
 import blogproject.blog.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +26,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardService boardService;
 
     @Autowired
     private BoardValidator boardValidator;
@@ -57,12 +62,14 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+    public String postForm(@Valid Board board, BindingResult bindingResult, Authentication authentication){
         boardValidator.validate(board, bindingResult);
         if (bindingResult.hasErrors()){
             return "board/form";
         }
-        boardRepository.save(board);
+        String username = authentication.getName();
+        boardService.save(username, board);
+  //      boardRepository.save(board);
         return "redirect:/board/list";
     }
 }
