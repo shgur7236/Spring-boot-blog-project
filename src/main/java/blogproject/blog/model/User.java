bbbc6df33824d1bb05de2b8aa339270de0d462f5
shgur7236 <shgurtns7236@naver.com>
2022-06-15 21:00:35 +0900

    fetchType Lazy

--- src/main/java/blogproject/blog/model/User.java
+++ src/main/java/blogproject/blog/model/User.java
@@ -1,5 +1,6 @@
 package blogproject.blog.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -17,6 +18,7 @@ public class User {
     private String password;
     private Boolean enabled;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(
             name = "user_role",
@@ -25,6 +27,7 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Board> boards = new ArrayList<>();
 }
