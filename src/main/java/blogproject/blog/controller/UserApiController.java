b7ce3c4a67138291dfd48ddd422746e5ab342851
shgur7236 <shgurtns7236@naver.com>
2022-06-15 20:30:04 +0900

    게시글 user 전체 삭제

--- src/main/java/blogproject/blog/controller/UserApiController.java
+++ src/main/java/blogproject/blog/controller/UserApiController.java
@@ -39,7 +39,9 @@ class UserApiController {
                 .map(user -> {
 //                    user.setTitle(newUser.getTitle());
 //                    user.setContent(newUser.getContent());
-                    user.setBoards(newUser.getBoards());
+                   // user.setBoards(newUser.getBoards());
+                    user.getBoards().clear();
+                    user.getBoards().addAll(newUser.getBoards());
                     for(Board board: user.getBoards()) {
                         board.setUser(user);
                     }
