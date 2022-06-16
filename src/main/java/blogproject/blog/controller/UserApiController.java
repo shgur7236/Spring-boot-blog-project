118b95644c3b3115da4d1d4f8d307fdd81150942
shgur7236 <shgurtns7236@naver.com>
2022-06-16 14:34:44 +0900

    log 찍어보기

--- src/main/java/blogproject/blog/controller/UserApiController.java
+++ src/main/java/blogproject/blog/controller/UserApiController.java
@@ -3,6 +3,7 @@ package blogproject.blog.controller;
 import blogproject.blog.model.Board;
 import blogproject.blog.model.User;
 import blogproject.blog.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 class UserApiController {
 
     @Autowired
@@ -17,7 +19,11 @@ class UserApiController {
 
     @GetMapping("/users")
     List<User> all() {
-        return repository.findAll();
+        List<User> users = repository.findAll();
+        log.debug("getBoards().size() 호출전");
+        log.debug("getBoards().size() 호출전 : {}", users.get(0).getBoards().size());
+        log.debug("getBoards().size() 호출후");
+        return users;
     }
 
     @PostMapping("/users")
